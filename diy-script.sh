58b74937494408e3c25805706942c2b2b78b74d0
tzsdysq <wwjsyl520@gmail.com>
2025-12-04 17:04:12 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -18,6 +18,7 @@ rm -rf feeds/luci/themes/luci-theme-netgear
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/applications/luci-app-netdata
 rm -rf feeds/luci/applications/luci-app-serverchan
+rm -rf feeds/luci/applications/luci-app-ddns-go
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
